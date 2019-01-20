12209a9b229a3cafb0fbd845ea3d427a2c590e68
hugoleboennec <hugo.leboennec@viacesi.fr>
2019-01-20 16:50:05 +0100

    Connexion Deconnexion et profil

---
+++ Authentification/connexion.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_menbre', 'root', '');
+
+if(isset($_POST['formconnexion'])) {
+   $mailconnect = htmlspecialchars($_POST['mailconnect']);
+   $mdpconnect = sha1($_POST['mdpconnect']);
+   if(!empty($mailconnect) AND !empty($mdpconnect)) {
+      $requser = $bdd->prepare("SELECT * FROM membres WHERE mail = ? AND MDP = ?");
+      $requser->execute(array($mailconnect, $mdpconnect));
+      $userexist = $requser->rowCount();
+      if($userexist == 1) {
+         $userinfo = $requser->fetch();
+         // var_dump($userinfo);
+         $_SESSION['id'] = $userinfo['ID'];
+         $_SESSION['nom'] = $userinfo['Nom'];
+         $_SESSION['prenom'] = $userinfo['Prenom'];
+         $_SESSION['mail'] = $userinfo['Mail'];
+         // var_dump($_SESSION);
+         // die();
+         header("Location: profil.php?id=".$_SESSION['id']);
+      } else {
+         $erreur = "Mauvais mail ou mot de passe !";
+      }
+   } else {
+      $erreur = "Tous les champs doivent être complétés !";
+   }
+}
+?>
+<html>
+   <head>
+      <title>Connexion</title>
+      <meta charset="utf-8">
+   </head>
+   <body>
+      <div align="center">
+         <h2>Connexion</h2>
+         <br /><br />
+         <form method="POST" action="">
+            <input type="email" name="mailconnect" placeholder="Mail" />
+            <input type="password" name="mdpconnect" placeholder="Mot de passe" />
+            <br /><br />
+            <input type="submit" name="formconnexion" value="Se connecter !" />
+         </form>
+         <?php
+         if(isset($erreur)) {
+            echo '<font color="red">'.$erreur."</font>";
+         }
+         ?>
+      </div>
+   </body>
+</html>
---
+++ Authentification/profil.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_menbre', 'root', '');
+
+if(isset($_GET['id']) AND $_GET['id'] > 0) {
+   $getid = intval($_GET['id']);
+   $requser = $bdd->prepare('SELECT * FROM membres WHERE ID = ?');
+   $requser->execute(array($getid));
+   $userinfo = $requser->fetch();
+?>
+<html>
+   <head>
+      <title><?php echo ($userinfo['Nom'] . ' ' . $userinfo['Prenom']); ?> - Profil</title>
+      <meta charset="utf-8">
+   </head>
+   <body>
+      <div align="center">
+         <h2>Profil de <?php echo ($userinfo['Nom'] . ' '. $userinfo['Prenom']); ?></h2>
+         <br /><br />
+         Nom = <?php echo $userinfo['Nom']; ?>
+         <br />
+         Prenom = <?php echo $userinfo['Prenom']; ?>
+         <br />
+         Mail = <?php echo $userinfo['Mail']; ?>
+         <br />
+         <?php
+         if(isset($_SESSION['id']) AND $userinfo['ID'] == $_SESSION['id']) {
+         ?>
+         <br />
+         <a href="editionprofil.php">Editer mon profil</a>
+         <a href="deconnexion.php">Se déconnecter</a>
+         <?php
+         }
+         ?>
+      </div>
+   </body>
+</html>
+<?php   
+} else header("Location: ");  // a completer
+?>
