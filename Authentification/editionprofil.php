594e8ef3185b2766d7a2d031b5f56324f7c06a82
hugoleboennec <hugo.leboennec@viacesi.fr>
2019-01-20 22:20:20 +0100

    Ajout de la gestion de l'avatar

--- Authentification/editionprofil.php
+++ Authentification/editionprofil.php
@@ -36,6 +36,32 @@ if(isset($_SESSION['id'])) {
          $msg = "Vos deux mdp ne correspondent pas !";
       }
    }
+
+   if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])) {
+      $tailleMax = 2097152;
+      $extensionsValides = array('jpg', 'jpeg', 'gif', 'png');
+      if($_FILES['avatar']['size'] <= $tailleMax) {
+         $extensionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
+         if(in_array($extensionUpload, $extensionsValides)) {
+            $chemin = "../img/Avatar/".$_SESSION['id'].".".$extensionUpload;
+            $resultat = move_uploaded_file($_FILES['avatar']['tmp_name'], $chemin);
+            if($resultat) {
+               $updateavatar = $bdd->prepare('UPDATE membres SET avatar = :avatar WHERE id = :id');
+               $updateavatar->execute(array(
+                  'avatar' => $_SESSION['id'].".".$extensionUpload,
+                  'id' => $_SESSION['id']
+                  ));
+               header('Location: profil.php?id='.$_SESSION['id']);
+            } else {
+               $msg = "Erreur durant l'importation de votre photo de profil";
+            }
+         } else {
+            $msg = "Votre photo de profil doit être au format jpg, jpeg, gif ou png";
+         }
+      } else {
+         $msg = "Votre photo de profil ne doit pas dépasser 2Mo";
+      }
+   }
 ?>
 <html>
    <head>
@@ -47,6 +73,14 @@ if(isset($_SESSION['id'])) {
          <h2>Edition de mon profil</h2>
             <form method="POST" action="" enctype="multipart/form-data">
                 <table>
+                    <tr>
+                        <td align="right">
+                           <label>Avatar :</label>
+                        </td>
+                        <td> 
+                           <input type="file" name="avatar"/>
+                        </td>
+                    </tr>
                     <tr>
                         <td align="right">
                             <label>Nom :</label>
