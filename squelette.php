bd3f599017bd9c4c8c6bd03759452e5e661ba3e6
AntoineCasenove <antoine.casenove@viacesi.fr>
2019-01-21 16:10:27 +0100

    hey

--- squelette.php
+++ squelette.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <?php include('src/site/dependances.php'); ?>
     <title>Skeleton</title>
 </head>
 <body>
+    HEY
     <header><?php include('src/site/header_interface.php'); ?></header>
     <main><?php include('src/site/main_interface.php'); ?></main>
 </body>
