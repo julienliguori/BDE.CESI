0ec7fda97717f5f1cd86010cf2ee04cadccb5826
hugoleboennec <hugo.leboennec@viacesi.fr>
2019-01-21 09:31:47 +0100

    bdd change

--- Authentification/connexion.php
+++ Authentification/connexion.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_menbre', 'root', '');
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
 
 if(isset($_POST['formconnexion'])) {
    $mailconnect = htmlspecialchars($_POST['mailconnect']);
--- Authentification/editionprofil.php
+++ Authentification/editionprofil.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_menbre', 'root', '');
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
 
 if(isset($_SESSION['id'])) {
    $requser = $bdd->prepare("SELECT * FROM membres WHERE id = ?");
