9d0362b60a2f526fdd2a77b1aa33deb66471d7ff
hugoleboennec <hugo.leboennec@viacesi.fr>
2019-01-21 16:14:10 +0100

    ddd

--- authentification/deconnexion.php
+++ authentification/deconnexion.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+
+$time_c=365*24*3600;
+setcookie('email','',time()+$time_c,null,null,false,true);
+setcookie('password','',time()+$time_c,null,null,false,true);
 $_SESSION = array();
 session_destroy();
 header("Location: connexion.php");
