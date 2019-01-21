799ac229f4ea43f0b73344806ebc15d07b8bd86e
AntoineCasenove <antoine.casenove@viacesi.fr>
2019-01-21 14:49:23 +0100

    interface nikelll

--- src/site/header_interface.php
+++ src/site/header_interface.php
@@ -1,5 +1,5 @@
-<!-- <img src="img/logo/bde.png" alt="Logo BDE" height="160" width="160" style="float: left;">
- -->
 <div class="black-top-fill-rectangle">&nbsp;</div>
 <div class="events-top-fill-rectangle">&nbsp;</div>
 <div class="black-rotated-fill-rectangle">&nbsp;</div>
+
+<img src="img/logo/bde.png" alt="Logo BDE" height="128" width="128" style="float: left; position:absolute; margin: -58px 0px 0px -237px">
