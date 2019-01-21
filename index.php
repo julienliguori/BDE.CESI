7f469108ff6a0723512875759e3580c1cbfbfe4f
AntoineCasenove <antoine.casenove@viacesi.fr>
2019-01-21 14:08:17 +0100

    dependances.php

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <!doctype html>
 <html lang='fr'>
 
-    <head> 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="Site Web pour le BDE du CESI.">
-        <meta name="author" content="Antoine Casenove, Hugo Le Boennec, Julien Liguori, Lorenzo Nouteau, Antonin Pretet">
-        <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" media="screen and (min-width: 900px)" href="widescreen.css">
-        <link rel="stylesheet" media="screen and (max-width: 600px)" href="smallscreen.css">
-
+    <head>
+        <?php include('src/site/dependances.php'); ?>
         <title>BDE.CESI</title>
     </head>
 
---
+++ src/site/dependances.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Site Web pour le BDE du CESI.">
+<meta name="author" content="Antoine Casenove, Hugo Le Boennec, Julien Liguori, Lorenzo Nouteau, Antonin Pretet">
+<link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" media="screen and (min-width: 900px)" href="widescreen.css">
+<link rel="stylesheet" media="screen and (max-width: 600px)" href="smallscreen.css">
