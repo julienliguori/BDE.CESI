f91d3efa8051443a8b1d846fe0d7b66c4f21846a
julienliguori <33621048+julienliguori@users.noreply.github.com>
2019-01-22 17:07:52 +0100

    main_interface

--- source/site/main_interface.php
+++ source/site/main_interface.php
@@ -1,4 +1,19 @@
 <div class="black-left-fill-rectangle">
     <p style = "margin-top: 100px; margin-left: 10px; color: white">^^ TU PEUX CLIQUER DESSUS ^^</p>
     <p style = "margin-top: 240px; margin-left: 40px; color: white">Devines..</p>&nbsp;
+    <?php 
+        /* var_dump(isset($_SESSION)); */
+        if($_SERVER['PHP_SELF'] == '/pages/boutique/boutique.php'){
+            echo 'Catégorie';
+        }
+        else if($_SERVER['PHP_SELF'] == '/pages/evenement/events.php'|| $_SERVER['PHP_SELF'] == '/pages/idees/idees.php'){
+            echo 'Calendrier';
+        }
+        else if($_SERVER['PHP_SELF'] == '/pages/index.php'){
+            echo 'Acceuil';
+        }
+        else{
+            echo $_SERVER['PHP_SELF'];
+        }
+    ?>
 </div>
