33f84a20825d02f39538245d69cbec97310709d4
AntoineCasenove <antoine.casenove@viacesi.fr>
2019-01-22 11:09:54 +0100

    connexion & inscription with interface integrated

--- source/site/header_interface.php
+++ source/site/header_interface.php
@@ -1,21 +1,22 @@
-<div class="black-top-fill-rectangle"><p style = "margin-left: 240px; color: white">Je taffe dessus @Antoine</p>
+<div class="black-top-fill-rectangle"><!-- <p style = "margin-left: 240px; color: white">Je taffe dessus @Antoine</p> -->
 
-<!--     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Menu</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-        <a class="nav-item nav-link" href="../pages/index.php">Accueil</a>
-        <a class="nav-item nav-link" href="../pages/squelette.php">Squelette</a>
-        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    <nav class="navbar-default navbar-expand-sm navbar-dark bg-dark">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="../../pages/index.php">Accueil</a>
+                <a class="nav-item nav-link" href="../../pages/squelette.php">Squelette</a>
+                <a class="nav-item nav-link" style="position: absolute; right: 80px;" href="../../source/authentification/connexion.php">Connexion</a>
+                <a class="nav-item nav-link" style="position: absolute; right: 10px;" href="../../source/authentification/inscription.php">Inscription</a>
+            </div>
         </div>
-    </div>
-    </nav> -->
+    </nav>
 
 &nbsp;</div>
 <div class="events-top-fill-rectangle"><p style = "margin-left: 240px; color: white">Tkt y'aura plein d'events ici :3</p>&nbsp;</div>
 <div class="black-rotated-fill-rectangle">&nbsp;</div>
 
-<a href="../pages/index.php"><img src="../source/img/logo/bde.png" alt="Logo BDE" height="128" width="128" style="float: left; position:absolute; margin: -58px 0px 0px -237px"></a>
+<a href="../../pages/index.php"><img src="../../source/img/logo/bde.png" alt="Logo BDE" height="128" width="128" style="float: left; position:absolute; margin: -58px 0px 0px -237px"></a>
