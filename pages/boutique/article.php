c9c8817e6ab2330067591e887e3119f03bada840
julienliguori <33621048+julienliguori@users.noreply.github.com>
2019-01-23 16:39:35 +0100

    pffffff

--- pages/boutique/article.php
+++ pages/boutique/article.php
@@ -2,23 +2,23 @@
 <html lang="fr">
 <head>
 
-    <?php   
+    <?php 
+    include 'boutique.php';  
         include_once('../../source/authentification/connexioncookie.php');
         include('../../source/site/dependances.php'); 
     ?>
     <title>Boutique</title>
 </head>
 <body>
-    <header><?php include('../../source/site/header_interface.php'); ?></header>
-    <main><?php include('../../source/site/main_interface.php'); ?></main>
-    <footer><?php include('../../source/site/footer_interface.php');?></footer>
+
     <?php
-    $json = '{"data":' . $_GET[$_SERVER['PHP_SELF']] . '}';//'{"data": [{"nom":"Tigre","prix":"12","quantite":"5","taille":"Little / Big","type":"truc","photo":"Tigre.jpg"},{"nom":"Pull","prix":"21","quantite":"2","taille":"1 / 2 / 3","type":"DES","photo":"Stylo.jpg"}]}';
+    $get =$_GET['http://localhost:8081/boutique/article/prix/=/12'];
+    $json = '{"data":' . $get . '}';//'{"data": [{"nom":"Tigre","prix":"12","quantite":"5","taille":"Little / Big","type":"truc","photo":"Tigre.jpg"},{"nom":"Pull","prix":"21","quantite":"2","taille":"1 / 2 / 3","type":"DES","photo":"Stylo.jpg"}]}';
         $parsed_json = json_decode($json,true);
 
         foreach ($parsed_json['data'] as $v) {
             ?>
-            <div>
+            <div>s
             <img src="..\..\source\img\boutique\ImgMemeFormat\<?php echo $v['photo']; ?>"/>
                 <div> <?php echo $v['nom']; ?> </div>
                 <div> Prix : <?php echo $v['prix']; ?> € </div>
