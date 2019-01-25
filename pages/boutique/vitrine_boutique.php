fb3db1c6c2282285a66da865c67f20c27fcce2c8
AntoineCasenove <antoine.casenove@viacesi.fr>
2019-01-25 13:30:38 +0100

    Vitrine Boutique

---
+++ pages/boutique/vitrine_boutique.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <?php   
+        include_once('../../source/authentification/connexioncookie.php');
+        include('../../source/site/dependances.php'); 
+    ?>
+    <title>Boutique ~ Vitrine</title>
+</head>
+<body>
+    <header><?php include('../../source/site/header_interface.php'); ?></header>
+    <footer><?php include('../../source/site/footer_interface.php');?></footer>
+</body>
+</html>
