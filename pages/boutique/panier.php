86b0a7f43199571f1e3e7e15b14074b709875d3a
julienliguori <33621048+julienliguori@users.noreply.github.com>
2019-01-25 16:02:47 +0100

    boutique

--- pages/boutique/panier.php
+++ pages/boutique/panier.php
@@ -3,14 +3,15 @@
 <head>
     
     <?php 
+        $time_c=365*24*3600;
         if(($_SERVER["REQUEST_URI"] == '/pages/boutique/panier.php')){
             if (isset($_COOKIE['article'])){
                 $article = $_COOKIE['article'];
             }else{
-                setcookie('article',0,time()+$time_c,null,null,false,true);
+                $article = '';
+                setcookie('article','',time()+$time_c,null,null,false,true);
             }
         }else{
-            $time_c=365*24*3600;
             $id = trim(strip_tags($_GET['condition']));
             $article = '';
             if (isset($_COOKIE['article'])){ 
@@ -45,6 +46,9 @@
   </thead>
   <tbody>
     <?php
+    if ($article == ''){
+        echo "tu n'a pas d'article";
+    }else{
     $articless = substr($article, 0, -1);
     $articles = explode(",",$articless);
 
@@ -54,7 +58,6 @@
         $condition = $fin;
         $url = "http://localhost:8081/boutique/article/$element/$signe/$condition";
         $json = '{"data": ' . file_get_contents($url) . ' }';
-    
         //echo($json);
         $parsed_json = json_decode($json,true);
         //var_dump($parsed_json);
@@ -64,7 +67,7 @@
                     <?php echo '
                             <tr>
                             <th scope="row">1</th>
-                            <td>' . $data["photo"] .' </td>
+                            <td><img src="/source/img/boutique/imgmemeformat/' . $data['photo'] . '" width=50px></img></td>
                             <td>' . $data["nom"] .'</td>
                             <td>' . $data["taille"] .'</td>
                             <td>' . $data["prix"] .'</td>
@@ -73,9 +76,14 @@
 
     
              <?php }
+             }
              } ?>   
    </tbody>
 </table>
+<a class="nav-link" href="../../pages/boutique/boutique.php">
+            <i class="far fa-times-circle feather feather-layers width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></i>
+              Retouner à la boutique
+            </a>
 </div>
 </div>
 <footer><?php include("../../source/site/footer_interface.php");?></footer>
