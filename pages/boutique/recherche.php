eef07e1b49f76076f181e453ecc944eb752f6a4b
Exochasse <38660196+Exochasse@users.noreply.github.com>
2019-01-25 11:10:09 +0100

    ff

--- pages/boutique/recherche.php
+++ pages/boutique/recherche.php
@@ -2,22 +2,25 @@
 
     $term = trim(strip_tags($_GET['term']));
 
-    /*echo $term;*/
+    echo $term;
 
-    $url = "http://localhost:8081/boutique/article/nom/like/$term";
-    $json = file_get_contents($url); 
+    // $url = "http://localhost:8081/boutique/article/nom/like/$term";
+    // $json = file_get_contents($url); 
     
-    $d_json=json_decode($json);
+    //$d_json=json_decode($json);
 
-    $d_json = $d_json[0]->{'nom'};
+    //$d_json = $d_json[0]->{'nom'};
 
-    $json_s="{'nom':'" . $d_json . "'}";
+    //$json_s="{'nom':'" . $d_json . "'}";
     
-    //echo $d_json;
+    // //echo $d_json;
 
-    foreach()
+    // foreach()
 
-    echo $json_s;
+    echo('{"nom": "poulet"}');
+
+    
+    //echo $json_s;
 
     flush();
 ?>
