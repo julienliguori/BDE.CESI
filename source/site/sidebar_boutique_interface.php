33b2ebc92b3889d1e60628d396b0d87ea9a550c0
AntoineCasenove <antoine.casenove@viacesi.fr>
2019-01-25 11:07:31 +0100

    sdfghjklm

--- source/site/sidebar_boutique_interface.php
+++ source/site/sidebar_boutique_interface.php
@@ -4,7 +4,7 @@
           <li class="nav-item">
             <a class="nav-link" href="#">
             <i class="far fa-times-circle feather feather-layers width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></i>
-              Suprimmer les filtres
+              Supprimer les filtres
             </a>
           </li>
         </ul>
