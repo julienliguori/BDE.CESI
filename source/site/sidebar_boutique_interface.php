7f2c2b9c37c25fe24ee43ad9f7b1cb24369b98d3
julienliguori <33621048+julienliguori@users.noreply.github.com>
2019-01-25 12:27:17 +0100

    change

--- source/site/sidebar_boutique_interface.php
+++ source/site/sidebar_boutique_interface.php
@@ -16,7 +16,7 @@
         </h6>
         <ul class="nav flex-column mb-2">
           <li class="nav-item">
-            <a class="nav-link" href="../../pages/boutique/produit.php?element=idArticle&signe=%3D%3C&condition=1">
+            <a class="nav-link" href="../../pages/boutique/produit.php?element=idArticle&signe=%3C%3D&condition=1">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
               1 à 20 €
             </a>
