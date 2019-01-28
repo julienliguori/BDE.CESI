26508a13b80e8fb01422167999545e33a67409bb
julienliguori <33621048+julienliguori@users.noreply.github.com>
2019-01-28 18:12:30 +0100

    post

--- pages/evenement/action.php
+++ pages/evenement/action.php
@@ -1,42 +1,56 @@
 <?php
-$bdd = new PDO("mysql:host=127.0.0.1;dbname=articles;charset=utf8", "root", "");
-if(isset($_GET['t'],$_GET['id']) AND !empty($_GET['t']) AND !empty($_GET['id'])) {
-   $getid = (int) $_GET['id'];
-   $gett = (int) $_GET['t'];
-   $sessionid = 5;
-   $check = $bdd->prepare('SELECT id FROM articles WHERE id = ?');
-   $check->execute(array($getid));
-   if($check->rowCount() == 1) {
-      if($gett == 1) {
-         $check_like = $bdd->prepare('SELECT id FROM likes WHERE id_article = ? AND id_membre = ?');
-         $check_like->execute(array($getid,$sessionid));
-         $del = $bdd->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
-         $del->execute(array($getid,$sessionid));
-         if($check_like->rowCount() == 1) {
-            $del = $bdd->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
-            $del->execute(array($getid,$sessionid));
-         } else {
-            $ins = $bdd->prepare('INSERT INTO likes (id_article, id_membre) VALUES (?, ?)');
-            $ins->execute(array($getid, $sessionid));
-         }
-         
-      } elseif($gett == 2) {
-         $check_like = $bdd->prepare('SELECT id FROM dislikes WHERE id_article = ? AND id_membre = ?');
-         $check_like->execute(array($getid,$sessionid));
-         $del = $bdd->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
-         $del->execute(array($getid,$sessionid));
-         if($check_like->rowCount() == 1) {
-            $del = $bdd->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
-            $del->execute(array($getid,$sessionid));
-         } else {
-            $ins = $bdd->prepare('INSERT INTO dislikes (id_article, id_membre) VALUES (?, ?)');
-            $ins->execute(array($getid, $sessionid));
-         }
-      }
-      header('Location: http://127.0.0.1/Tutos_PHP/Articles/article.php?id='.$getid);
-   } else {
-      exit('Erreur fatale. <a href="http://127.0.0.1/Tutos_PHP/Articles/">Revenir à l\'accueil</a>');
-   }
-} else {
-   exit('Erreur fatale. <a href="http://127.0.0.1/Tutos_PHP/Articles/">Revenir à l\'accueil</a>');
-}
+session_start(); 
+
+$idEvent = trim(strip_tags($_GET['id']));
+$nbParti = trim(strip_tags($_GET['nbParti']));
+$nbPartici = intval($nbParti);
+$nbParticipant = $nbPartici + 1;
+var_dump($nbParticipant);
+
+$array2 = array(
+   'nbParticipant' => $nbParticipant,
+);
+
+$array2JSON = json_encode($array2);
+
+$options2 = array(
+   'http'=> array(
+       'method' => 'PUT',
+       'header'=> "Content-Type: application/json\r\n" .
+                  "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluQm91dGlxdWUifSwiaWF0IjoxNTQ4NjMwMjQyfQ.v6eCHbT4zqZ-Ymv8rBFtncRLjFJZbFcZvHudfoGUM9g\r\n",
+       'content' => $array2JSON
+   )
+);
+
+$context2 = stream_context_create($options2);
+var_dump($idEvent);
+
+
+
+
+    $nom = $_SESSION['nom'] . '_' . $_SESSION['prenom'];
+
+         $array = array(
+            'idEvenement' => $idEvent,
+            'nomClient' => $nom,
+        );
+      
+      //Encode array 
+        $arrayJSON = json_encode($array);
+
+      //params convertion
+  $options = array(
+   'http'=> array(
+       'method' => 'POST',
+       'header'=> "Content-Type: application/json\r\n" .
+                  "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluQm91dGlxdWUifSwiaWF0IjoxNTQ4NjMwMjQyfQ.v6eCHbT4zqZ-Ymv8rBFtncRLjFJZbFcZvHudfoGUM9g\r\n",
+       'content' => $arrayJSON
+   )
+);
+
+$context = stream_context_create($options);
+header('Location: http://bde.cesi/pages/evenement/events.php');
+return file_get_contents("http://localhost:8081/evenement/evenement/idEvenement/=/$idEvent", false, $context2);
+return file_get_contents('http://localhost:8081/evenement/participer/', false, $context);
+
+?>
