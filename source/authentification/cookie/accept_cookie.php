ab1c7d9dddd3b81d81f009ed0683b5c35fb9e640
APZEY <antonin.pretet@orange.fr>
2019-01-28 18:34:22 +0100

    CookieUp&Down

--- source/authentification/cookie/accept_cookie.php
+++ source/authentification/cookie/accept_cookie.php
@@ -1,3 +1,10 @@
 <?php
 setcookie('accept_cookie', true, time() + 365*24*3600, '/', null, false, true);
+
+if(isset($_SERVER['HTTP_REFERER']) AND !empty($_SERVER['HTTP_SERVER'])) {
+    header('Location:' .$_SERVER['HTTP_REFERER']);
+}    else {
+        header('Location:http://bde.cesi/pages/home.php');
+    }
+//header('Location: #');
 ?>
--- source/authentification/cookie/page.php
+++ source/authentification/cookie/page.php
@@ -4,9 +4,8 @@
 
 if($showcookie==true) { ?>
     <div class="cookie-alert">
-       En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour vous proposer des contenus et services adaptés à vos centres d’intérêts.<br /><a href="./accept_cookie.php">OK</a>
+       En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour vous proposer des contenus et services adaptés à vos centres d’intérêts.<br /><a href="http://bde.cesi/source/authentification/cookie/accept_cookie.php">OK</a>
     </div>
-<?php } 
-else echo 'boq';
+<?php }
 ?>
     
